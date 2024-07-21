2648459c3be09075d73e4e1ab12a97ad8a429e06
Magnus Carnifex <spectre@magnuscarnifex.com>
2024-07-21 17:09:14 -0400

    use starter code

---
+++ DEV.md
@@ -0,0 +1,6 @@
+# Background
+
+Basing work off this [codesandbox example](https://codesandbox.io/s/tiptap-test-zdxmm).
+
+# Notes
+Vue CLI is being used to scaffold the project, think of it less of a cli tool and more of a framework around which we have the server/transpilation/etc.
---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <TestPage />
+  </div>
+</template>
+
+<script>
+import TestPage from './components/TestPage.vue';
+
+export default {
+  name: 'App',
+  components: {
+    TestPage,
+  },
+};
+</script>
+
+<style>
+@import 'https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css';
+
+#app {
+  font-family: Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
